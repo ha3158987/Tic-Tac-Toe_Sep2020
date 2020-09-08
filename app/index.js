2a5d50bb9d41fef390398a97592d1e91b9fbee91
Hannah <ha3158987@gmail.com>
2020-09-08 22:46:42 +0900

    Upload framework

--- app/index.js
+++ app/index.js
@@ -3,6 +3,6 @@
 // ================================
 
 // You can delete this code..
-setTimeout(function () {
-  alert('Getting Started? Look for app/index.js..');
-}, 500);
+// setTimeout(function () {
+//   alert('Getting Started? Look for app/index.js..');
+// }, 500);
